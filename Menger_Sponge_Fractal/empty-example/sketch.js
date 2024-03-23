b18a0bbdb3f72402a9d885c803c96840ae200714
erenkara <errennkaaraa@hotmail.com>
2024-03-23 17:39:07 +0300

    new Day new Algorithms

---
+++ Menger_Sponge_Fractal/empty-example/sketch.js
@@ -0,0 +1,60 @@
+class Box {
+    x;
+    y;
+    z;
+    
+    r; // size;
+    
+
+    constructor(x,y,z, r){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+
+        this.r=r;
+        
+    }
+    Show(){
+        
+    }
+
+}
+
+var a = 0;
+
+var sponge = [];
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  // as of p5.js 0.6.0, normal material is no longer the default and
+  // has to be explicitly selected.
+  normalMaterial();
+
+  // An array of Box objects
+  // Star with one
+  var b = new Box(0, 0, 0, 200);
+  sponge.push(b);
+}
+
+function mousePressed() {
+  // Generate the next set of boxes
+  var next = [];
+  for (var i = 0; i < sponge.length; i++) {
+    var b = sponge[i];
+    var newBoxes = b.generate();
+    next = next.concat(newBoxes);
+  }
+  sponge = next;
+}
+
+function draw() {
+  background(0);
+  rotateX(a);
+  rotateY(a * 0.4);
+  rotateZ(a * 0.1);
+  // Show what you've got!
+  for (var i = 0; i < sponge.length; i++) {
+    sponge[i].show();
+  }
+  a += 0.01;
+}
